ffde930f5bb66da4675aae9147065bc807411b40
christianburkard <51155407+christianburkard@users.noreply.github.com>
2020-01-23 15:03:44 +0100

    Update TinyLev.py

--- TinyLev.py
+++ TinyLev.py
@@ -1014,12 +1014,12 @@ class objectDetection():
                     print("PiX coordinate: {:.2f}".format(PixCoordX), "  PiY coordinate: {:.2f}".format(PixCoordY))
 
                     # only proceed if the radius meets a minimum size
-                    if (radius > 10 and radius < 60):
-#                    if (radius > 0):
+#                    if (radius > 10 and radius < 60):
+                    if (radius > 0):
                         # draw the circle and centroid on the frame,
                         # then update the list of tracked points
-                        cv2.circle(frame, (int(x), int(y)), int(radius), (15, 186, 2), 10)
-                        cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                        cv2.circle(frame0, (int(x), int(y)), int(radius), (15, 186, 2), 10)
+                        cv2.circle(frame0, center, 5, (0, 0, 255), -1)
 
                 # update the points queue
                     try:
